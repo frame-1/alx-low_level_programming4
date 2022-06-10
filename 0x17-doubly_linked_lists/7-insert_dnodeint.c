5210fe45759a05e493b7502bdd849d8609eada21
frame-1 <muthee031@gmail.com>
2022-06-09 17:10:06 -0700

    Tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,7 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 						new->next = head->next;
 						new->prev = head;
 						head->next->prev = new;
-					        head->next = new;
+						head->next = new;
 					}
 				}
 				break;
